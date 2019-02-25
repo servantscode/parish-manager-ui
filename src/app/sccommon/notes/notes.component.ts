0e0c714fcd0fb9c9149229f8419bd7d9c4c6dc3a
Greg Leitheiser <greg@servantscode.org>
2019-02-25 15:45:54 -0600

    Notes on families and ministries; Editing notes.

--- src/app/sccommon/notes/notes.component.ts
+++ src/app/sccommon/notes/notes.component.ts
@@ -1,9 +1,14 @@
-import { Component, OnChanges, Input } from '@angular/core';
+import { Component, OnChanges, Input, HostListener } from '@angular/core';
 import { FormBuilder, Validators, ValidatorFn, AbstractControl } from '@angular/forms';
 
 import { LoginService } from '../services/login.service';
 import { NoteService } from '../services/note.service';
 import { Note } from '../note';
+
+export enum KEY_CODE {
+  ENTER = 13,
+  ESCAPE = 27
+}
 
 @Component({
   selector: 'app-notes',
@@ -15,17 +20,18 @@ export class NotesComponent implements OnChanges {
   @Input() resourceType: string;
   @Input() resourceId: number;
 
-  notes: Note[];
-  formOpen = false;
+  notes: Note[] = [];
+  highlighted: Note;
+  moreNotes = false;
 
   form = this.fb.group({
+      id: [''],
       creatorId: [this.loginService.getUserId(), Validators.required],
       private: [false, Validators.required],
       resourceType: [this.resourceType, Validators.required],
       resourceId: [this.resourceId, Validators.required],
       note: ['', Validators.required]
     });
-  
 
   constructor(private fb: FormBuilder,
               private loginService: LoginService,
@@ -39,39 +45,75 @@ export class NotesComponent implements OnChanges {
     this.form.get('resourceId').setValue(this.resourceId);
   }
 
-  openForm(): void {
-    this.formOpen = true;
+  @HostListener('window:keyup', ['$event'])
+  keyEvent(event: KeyboardEvent) {    
+    // if (event.keyCode === KEY_CODE.ENTER) {
+    //   this.save();
+    // }
+
+    if (event.keyCode === KEY_CODE.ESCAPE) {
+      this.resetForm();
+    }
+  }
+
+  loadMore(): void {
+    this.getNotes();
+  }
+
+  edit(note: Note): void {
+    this.form.get("note").setValue(note.note);
+    this.form.get("private").setValue(note.private);
+    this.form.get("id").setValue(note.id);
   }
 
   save(): void {
-    // if(this.ministry.id > 0) {
-    //   if(!this.loginService.userCan('ministry.update'))
-    //     this.goBack();
-    //   this.ministryService.update(this.ministryForm.value).
-    //     subscribe(ministry => {
-    //       this.ministry = ministry;
-    //       this.editMode = false;
-    //       this.router.navigate(['ministry', 'detail', ministry.id]);
-    //     });
-    // } else {
-    if(!this.loginService.userCan('note.create'))
-      return;
-    else if(this.form.get('private').value && !this.loginService.userCan('private.note.create'))
-      return;
 
-    this.noteService.create(this.form.value).
-      subscribe(note => {
-        this.notes.unshift(note);
-        this.form.get("note").setValue("");
-        this.form.get("private").setValue(false);
-        this.formOpen = false;
-      });      
-    // }
+    if(this.form.get("id").value > 0) {
+      if(!this.loginService.userCan('note.update'))
+        return;
+      else if(this.form.get('private').value && !this.loginService.userCan('private.note.update'))
+        return;
+
+      var note = this.form.value;
+      note.note = note.note.trim();
+      this.noteService.update(note).
+        subscribe(note => {
+          var replacePoint = this.notes.findIndex(n => n.id == note.id);
+          this.notes.splice(replacePoint, 1, note);
+          this.resetForm();
+        });
+    } else {
+      if(!this.loginService.userCan('note.create'))
+        return;
+      else if(this.form.get('private').value && !this.loginService.userCan('private.note.create'))
+        return;
+
+      var note = this.form.value;
+      note.note = note.note.trim();
+      this.noteService.create(note).
+        subscribe(note => {
+          this.notes.unshift(note);
+          this.resetForm();
+        });      
+    }
+  }
+
+  highlight(note: Note): void {
+    this.highlighted = note;
   }
 
   // ----- Private -----
 
   private getNotes() {
-    this.noteService.getPage(0, 10, this.resourceType + ":" + +this.resourceId).subscribe(resp => this.notes=resp.results);
+    this.noteService.getPage(this.notes.length, 10, this.resourceType + ":" + +this.resourceId).subscribe(resp => {
+        resp.results.forEach(note => this.notes.push(note));
+        this.moreNotes = resp.totalResults > this.notes.length;
+      });
+  }
+
+  private resetForm() {
+    this.form.get("note").reset();
+    this.form.get("private").reset();
+    this.form.get("id").reset();
   }
 }
