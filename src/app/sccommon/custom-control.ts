2d81ae4ac0f2ed98dd054d03b0c086b459b2e998
Greg Leitheiser <greg@servantscode.org>
2019-11-14 11:47:35 -0600

    Volunteer checkin tokens now expire at end of shift; BUG: show report download for people and families

--- src/app/sccommon/custom-control.ts
+++ src/app/sccommon/custom-control.ts
@@ -2,13 +2,19 @@ import { Identifiable } from 'sc-common';
 
 export class CustomControl<T extends Identifiable> {
 
-  constructor(type: string, action: (item:T) => void) {
+  constructor(type: string, action: (item:T) => void, enabled?: (item:T) => boolean) {
     this.type = type;
     this.action = action;
+    this.enabled = enabled;
   }
 
   type: string;
   action: (item:T) => void = null;
+  enabled: (item:T) => boolean = null;
+
+  isEnabled(item:T): boolean {
+    return this.enabled? this.enabled(item): true;
+  }
 
   invoke(item:T) {
     this.action(item);
--- src/app/volunteer/checkin/checkin.component.ts
+++ src/app/volunteer/checkin/checkin.component.ts
@@ -1,7 +1,14 @@
 import { Component, OnInit } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+import { Subject } from 'rxjs';
+import { isAfter } from 'date-fns';
+
+import { CustomControl } from '../../sccommon/custom-control';
 
 import { CheckinService } from '../services/checkin.service';
 import { CheckinDialogComponent } from '../checkin-dialog/checkin-dialog.component';
+
+import { Checkin } from '../checkin'
 
 @Component({
   selector: 'app-checkin',
@@ -14,7 +21,30 @@ export class CheckinComponent implements OnInit {
 
   activeCheckins = this.checkinService.getActiveCheckins.bind(this.checkinService);
 
-  constructor(public checkinService: CheckinService) { }
+  refreshList = new Subject<any>();
+
+  controls: CustomControl<Checkin>[] = [
+    new CustomControl("checkin", 
+      (item: Checkin) => {
+      if(!item)
+        return;
+
+      const openDialog = this.dialog.open(CheckinDialogComponent, {
+        width: '400px',
+        data: {"item": {'personId': item.personId}}
+      });
+
+      openDialog.afterClosed().subscribe(result => {
+          this.refreshList.next();
+        });
+    },
+    (item: Checkin) => {
+      return isAfter(new Date(), item.expiration);
+    })
+  ];
+
+  constructor(private dialog: MatDialog,
+              public checkinService: CheckinService) { }
 
   ngOnInit() {
   }
